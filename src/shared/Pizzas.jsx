cc9c91e8e993980acaf094712892419df9b15c11
KKainan <kainanamajiki@gmail.com>
2024-08-09 10:01:52 -0300

    Tudo acabará em pizza

---
+++ src/shared/Pizzas.jsx
@@ -0,0 +1,41 @@
+import mussarela from "../assets/images/mussarela.png"
+import Calabresa from "../assets/images/Calabresa.png"
+import Atum from "../assets/images/Atum.png"
+import Portuguesa from "../assets/images/Portuguesa.png"
+import Frango from "../assets/images/Frango.png"
+
+const Pizzas = [
+    
+{
+    "id": 1,
+    "nome": "Mussarela",
+    "descricao": "Molho de tomate e mussarela.",
+    "imagem": mussarela
+},
+{
+    "id": 2,
+    "nome": "Frango com catupiry",
+    "descricao": "Molho de tomate, mussarela, frango desfiado e catupiry.",
+    "imagem": Frango
+},
+{
+    "id": 3,
+    "nome": "Portuguesa",
+    "descricao": "Molho de tomate, mussarela, presunto, ovo, cebola, azeitona e orégano.",
+    "imagem": Portuguesa
+},
+{
+    "id": 4,
+    "nome": "Calabresa",
+    "descricao": "Molho de tomate, mussarela, calabresa e cebola.",
+    "imagem": Calabresa
+},
+{
+    "id": 5,
+    "nome": "Atum",
+    "descricao": "Molho de tomate, mussarela, atum e tomate.",
+    "imagem": Atum
+},
+]
+
+export default Pizzas;
